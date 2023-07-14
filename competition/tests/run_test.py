0da9e1ce09efb7038bb2b6fc223b334aa0bd7438
spanovs01 <spanovs01frag@gmail.com>
2023-07-14 14:46:30 +0300

    run_test parses params from class Competition

--- competition/tests/run_test.py
+++ competition/tests/run_test.py
@@ -18,26 +18,26 @@ class RunTest(Competition):
                         self.motion.sim.simx_opmode_oneshot)  # Шея Наклон
             for j in range(20):
                 self.motion.sim.simxSynchronousTrigger(self.motion.clientID)
-        if self.button == 1:             # fast step
-            number_Of_Cycles = 30 #30
-            self.motion.simThreadCycleInMs = 10
-            self.motion.amplitude = 32 #32
-            self.motion.fr1 = 4 # 4
-            self.motion.fr2 = 9 # 10
-            ##self.motion.initPoses = self.motion.fr2 
-            self.motion.gaitHeight = 170 # 190
-            self.motion.stepHeight = 30  # 20
-            stepLength = 50 #88
-        if self.button == 2  :   
-            number_Of_Cycles = 30 #30
-            self.motion.simThreadCycleInMs = 10
-            self.motion.amplitude = 0 #32
-            self.motion.fr1 = 8 # 4
-            self.motion.fr2 = 12
-            ##self.motion.initPoses = self.motion.fr2 
-            self.motion.gaitHeight = 160
-            self.motion.stepHeight = 40  # 20
-            stepLength = 40
+        # if self.button == 1:             # fast step
+        #     number_Of_Cycles = 30 #30
+        #     self.motion.simThreadCycleInMs = 10
+        #     self.motion.amplitude = 32 #32
+        #     self.motion.fr1 = 4 # 4
+        #     self.motion.fr2 = 9 # 10
+        #     ##self.motion.initPoses = self.motion.fr2 
+        #     self.motion.gaitHeight = 170 # 190
+        #     self.motion.stepHeight = 30  # 20
+        #     self.stepLength = 50 #88
+        # if self.button == 2  :   
+        #     number_Of_Cycles = 30 #30
+        #     self.motion.simThreadCycleInMs = 10
+        #     self.motion.amplitude = 0 #32
+        #     self.motion.fr1 = 8 # 4
+        #     self.motion.fr2 = 12
+        #     ##self.motion.initPoses = self.motion.fr2 
+        #     self.motion.gaitHeight = 160
+        #     self.motion.stepHeight = 40  # 20
+        #     self.stepLength = 40
         sideLength = 0
         #self.motion.first_Leg_Is_Right_Leg = False
         if self.motion.first_Leg_Is_Right_Leg: invert = -1
@@ -48,9 +48,9 @@ class RunTest(Competition):
             for cycle in range(number_Of_Cycles):
                 if cycle > 1: self.glob.camera_ON = True
                 if not self.motion.falling_Flag == 0: break
-                stepLength1 = stepLength
-                if cycle ==0 : stepLength1 = stepLength/3
-                if cycle ==1 : stepLength1 = stepLength/3 * 2
+                stepLength1 = self.stepLength
+                if cycle ==0 : stepLength1 = self.stepLength/3
+                if cycle ==1 : stepLength1 = self.stepLength/3 * 2
                 self.motion.refresh_Orientation()
                 rotation = 0 + invert * self.motion.body_euler_angle['yaw'] * 1.0
                 #if rotation < 0: rotation *= 5
@@ -58,28 +58,28 @@ class RunTest(Competition):
                 #rotation = 0
                 self.motion.walk_Cycle(stepLength1,sideLength, 0,cycle, number_Of_Cycles)
                 if self.motion.i_see_ball:
-                    stepLength1 = stepLength/3 * 2
+                    stepLength1 = self.stepLength/3 * 2
                     self.motion.refresh_Orientation()
                     rotation = 0 + invert * self.motion.body_euler_angle['yaw'] * 1.0
                     rotation = self.motion.normalize_rotation(rotation)
                     self.motion.walk_Cycle(stepLength1,sideLength, rotation, 1, 3)
-                    stepLength1 = stepLength/3
+                    stepLength1 = self.stepLength/3
                     self.motion.refresh_Orientation()
                     rotation = 0 + invert * self.motion.body_euler_angle['yaw'] * 1.0
                     rotation = self.motion.normalize_rotation(rotation)
                     self.motion.walk_Cycle(stepLength1,sideLength, rotation, 1, 3)
-                    stepLength1 = -stepLength/3
+                    stepLength1 = -self.stepLength/3
                     self.motion.refresh_Orientation()
                     rotation = 0 + invert * self.motion.body_euler_angle['yaw'] * 1.0
                     rotation = self.motion.normalize_rotation(rotation)
                     self.motion.walk_Cycle(stepLength1,sideLength, rotation, 1, 3)
-                    stepLength1 = -stepLength/3 * 2
+                    stepLength1 = -self.stepLength/3 * 2
                     self.motion.refresh_Orientation()
                     rotation = 0 + invert * self.motion.body_euler_angle['yaw'] * 1.0
                     rotation = self.motion.normalize_rotation(rotation)
                     self.motion.walk_Cycle(stepLength1,sideLength, rotation, 1, 3)
                     for back_cycle in range(5):
-                        stepLength1 = -stepLength
+                        stepLength1 = -self.stepLength
                         self.motion.refresh_Orientation()
                         rotation = 0 + invert * self.motion.body_euler_angle['yaw'] * 1.0
                         rotation = self.motion.normalize_rotation(rotation)
